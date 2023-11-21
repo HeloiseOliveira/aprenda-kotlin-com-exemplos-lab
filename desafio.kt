09e90c918bd85a666d43540946fab3f29c22dc43
Heloise Oliveira <113391588+HeloiseOliveira@users.noreply.github.com>
2023-11-21 14:49:25 -0300

    concluido

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,46 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, private val conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+    }
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun exibirConteudos() {
+        println("Conteúdos da formação $nome:")
+        conteudos.forEach { println("- ${it.nome} (${it.duracao} minutos)") }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criação de objetos para teste
+    val usuario1 = Usuario("Usuário 1")
+    val usuario2 = Usuario("Usuário 2")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 60)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 90)
+
+    val formacaoKotlin = Formacao("Formação Kotlin")
+    formacaoKotlin.adicionarConteudo(conteudo1)
+    formacaoKotlin.adicionarConteudo(conteudo2)
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    formacaoKotlin.exibirConteudos()
+
+    println("Inscritos na formação ${formacaoKotlin.nome}:")
+    formacaoKotlin.inscritos.forEach { println("- ${it.nome}") }
 }
+
